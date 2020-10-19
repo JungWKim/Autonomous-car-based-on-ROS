5320bd764fcbca95010d1d5e461f49eb071d2111
JWooKim <lionmini@naver.com>
2020-10-20 06:13:38 +0900

    .

--- Embedded/JetsonTX2/withoutROS/serial/main.c
+++ Embedded/JetsonTX2/withoutROS/serial/main.c
@@ -1,8 +1,8 @@
-#include "serial.h"
+#include "serial.h" 
 
 int fd; // 시리얼포트 파일핸들
-int recvBuffer = 101;
-int sendBuffer;
+int recvBuffer;
+int sendBuffer=101;
 int rdcnt; 
 
 void write_port()
@@ -10,12 +10,12 @@ void write_port()
     while(1)
     {
         // 데이타를 읽어온다.
-        if((rdcnt = write( fd, &recvBuffer, sizeof(recvBuffer))) < 0)
+        if((rdcnt = write( fd, &sendBuffer, sizeof(sendBuffer))) < 0)
         {
             perror("[failed] write");
             exit(1);
         }
-        cout << "received data :" << recvBuffer << endl;
+        printf("send data : %d\n", sendBuffer);
         sleep(1);
     }
 
@@ -26,12 +26,12 @@ void read_port()
     while(1)
     {
         // 데이타를 읽어온다.
-        if((rdcnt = read(fd, &sendBuffer, sizeof(sendBuffer))) < 0)
+        if((rdcnt = read(fd, &recvBuffer, sizeof(recvBuffer))) < 0)
         {
             perror("[failed] read");
             exit(1);
         }
-        cout << "send data :" << sendBuffer << endl;
+        printf("read data : %d\n", recvBuffer);
         sleep(1);
     }
 
@@ -43,7 +43,9 @@ int main(int argc, char **argv) {
     char dev_name[128]; // 시리얼포트 노드파일 이름
     char cc; // 데이타 버퍼
     int i = 0;
-    int thread_id;
+    int thread_id, status;
+    char p1[10] = "thread_1";
+    char p2[10] = "thread_2";
     pthread_t p_thread[2];
 
     if ( argc != 3 )
@@ -68,22 +70,25 @@ int main(int argc, char **argv) {
         perror("[failed] open serial");
         return -2; 
     }
-    std::cout << "open serial socket" << std::endl;
+    printf("open serial socket\n");
 
-    thread_id = pthread_create(&p_thread[0], NULL, t_function, (void *)p1);
-    if (thr_id < 0)
+    thread_id = pthread_create(&p_thread[0], NULL, write_port, (void *)p1);
+    if (thread_id < 0)
     {
         perror("thread create error : ");
         exit(0);
     }
 
     // ② 2번 쓰레드 생성
-    thr_id = pthread_create(&p_thread[1], NULL, t_function, (void *)p2);
-    if (thr_id < 0)
+    thread_id = pthread_create(&p_thread[1], NULL, read_port, (void *)p2);
+    if (thread_id < 0)
     {
         perror("thread create error : ");
         exit(0);
     }
+
+    pthread_join(p_thread[0], (void **)&status);
+    pthread_join(p_thread[1], (void **)&status);
 
     // 시리얼 포트를 닫는다.
     close_serial( fd );
