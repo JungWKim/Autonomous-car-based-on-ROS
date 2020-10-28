0f59312529bac1f127a7d1da4cfcf33db1600d60
JWooKim <lionmini@naver.com>
2020-10-28 21:26:51 +0900

    add aeb code

--- Embedded/Arduino/aeb/aeb.ino
+++ Embedded/Arduino/aeb/aeb.ino
@@ -1,7 +1,7 @@
 #include <math.h>
 #include <MsTimer2.h>
 
-#define encoderL 10
+#define encoderL 18
 #define encoderR 21
 
 #define EA 6
@@ -26,16 +26,6 @@ volatile int error;
 
 volatile double Pcontrol;
 
-void speed_limit()
-{
-   if(vel_L > 250) vel_L = 250;
-   else if(vel_L < 50) vel_L = 50;
-              
-   if(vel_R > 250) vel_R = 250;
-   else if(vel_R < 50) vel_R = 50;
-                          
-}
-
 void speedSetup(int left, int right)
 {
     analogWrite(EA, left);
@@ -47,21 +37,21 @@ void speedCalibration()
     rpmL = (int)((pulseCountL / ppr) * (60.0 / 0.5));
     rpmR = (int)((pulseCountR / ppr) * (60.0 / 0.5));
 
-    error = rpmL - rpmR;
-    Pcontrol = Kp * abs(error);
-    if(error < 0) vel_L += Pcontrol;
-    else if(error > 0) vel_R += Pcontrol;
-    speed_limit();
-    speedSetup(vel_L, vel_R);
+//    error = rpmL - rpmR;
+//    Pcontrol = Kp * abs(error);
+//    if(error < 0) vel_L += Pcontrol;
+//    else if(error > 0) vel_R += Pcontrol;
+//    speed_limit();
+//    speedSetup(vel_L, vel_R);
     
-    Serial.print("Left rpm: ");
-    Serial.println(rpmL);
-    Serial.print("Right rpm: ");
-    Serial.println(rpmR);
-    Serial.print("left speed : ");
-    Serial.println(vel_L);
-    Serial.print("right speed : ");
-    Serial.println(vel_R);
+//    Serial.print("Left rpm: ");
+//    Serial.println(rpmL);
+//    Serial.print("Right rpm: ");
+//    Serial.println(rpmR);
+//    Serial.print("left speed : ");
+//    Serial.println(vel_L);
+//    Serial.print("right speed : ");
+//    Serial.println(vel_R);
     
     pulseCountL = 0;
     pulseCountR = 0;
@@ -93,14 +83,15 @@ void setup()
     MsTimer2::start();
     speedSetup(170, 170);//initial speed
 
-    digitalWrite(A2, LOW);
-    digitalWrite(A1, HIGH);
-    digitalWrite(B4, LOW);
-    digitalWrite(B3, HIGH);
+    digitalWrite(A2, HIGH);
+    digitalWrite(A1, LOW);
+    digitalWrite(B4, HIGH);
+    digitalWrite(B3, LOW);
 }
 
 void loop()
 {
+    //speedSetup(170, 170);//initial speed
     digitalWrite(trig, LOW);
     delayMicroseconds(2);
     digitalWrite(trig, HIGH);
@@ -110,51 +101,56 @@ void loop()
     obstacle_distance = ((float)(340 * reflect_duration) / 10000) / 2;
     rpm = (rpmL + rpmR) / 2;
     velocity = (rpm * 6.6 * 3.141592) / 60;
-    if(velocity == 0)
+    ttc = (obstacle_distance / velocity) + 1.0;
+    while(obstacle_distance <= 30)
     {
-        ttc = 1.0;
+      digitalWrite(trig, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trig, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trig, LOW);
+      reflect_duration = pulseIn(echo, HIGH);
+      obstacle_distance = ((float)(340 * reflect_duration) / 10000) / 2;
+      rpm = (rpmL + rpmR) / 2;
+      velocity = (rpm * 6.6 * 3.141592) / 60;
+      ttc = (obstacle_distance / velocity) + 1.0;
+      if(ttc <= 3)
+      {
+          speedSetup(0, 0);
+      }
+      else if(ttc <= 4)
+      {
+          speedSetup(50, 50);
+      }
+      else if(ttc <= 5)
+      {
+          speedSetup(80, 80);
+      }
+      else
+      {
+        break;
+      }
+      Serial.print("distance : ");
+      Serial.println(obstacle_distance);
+      Serial.print("velocity : ");
+      Serial.println(velocity);
+      Serial.print("ttc : ");
+      Serial.println(ttc);
     }
-    else
-    {
-        ttc = (obstacle_distance / velocity) + 1.0;
-    }
-    
-    if(ttc <= 2.0)
-    {
-        vel_L = 0;
-        vel_R = 0;
-        speedSetup(vel_L, vel_R);
-    }
-    else if(ttc <= 3.0)
-    {
-        vel_L = 50;
-        vel_R = 50;
-        speedSetup(vel_L, vel_R);
-    }
-    else if(ttc <= 4.0)
-    {
-        vel_L = 70;
-        vel_R = 70;
-        speedSetup(vel_L, vel_R);
-    }
-    else if(ttc <= 5.0)
-    {
-        vel_L = 100;
-        vel_R = 100;
-        speedSetup(vel_L, vel_R);
-    }
-    else if(ttc <= 6.0)
-    {
-        vel_L = 140;
-        vel_R = 140;
-        speedSetup(vel_L, vel_R);
-    }
-    else
-    {
-        vel_L = 170;
-        vel_R = 170;
-        speedSetup(vel_L, vel_R);
-    }
-    Serial.print("distance :");
+    speedSetup(170, 170);
+//    else if(ttc <= 5)
+//    {
+//        speedSetup(70, 70);
+//    }
+
+    Serial.print("distance : ");
     Serial.println(obstacle_distance);
+//    Serial.print("rpmL : ");
+//    Serial.println(rpmL);
+//    Serial.print("rpmR : ");
+//    Serial.println(rpmR);
+      Serial.print("velocity : ");
+      Serial.println(velocity);
+      Serial.print("ttc : ");
+      Serial.println(ttc);
 }
--- Embedded/Arduino/sonar/sonar.ino
+++ Embedded/Arduino/sonar/sonar.ino
@@ -1,5 +1,7 @@
-#define trig 37
-#define echo 33
+#define trig 5
+#define echo 6
+
+// detectable distance : 2 ~ 230 cm
 
 float duration, distance;
 
@@ -18,7 +20,43 @@ void loop()
     delayMicroseconds(10);
     digitalWrite(trig, LOW);
     duration = pulseIn(echo, HIGH);
-    distance = ((float)(340 * duration) / 10000) / 2;
+    distance = ((340 * duration) / 10000) / 2;
     Serial.print("distance :");
     Serial.println(distance);
+    delay(1000);
 }
+
+//const int pingPin = 5; // Trigger Pin of Ultrasonic Sensor
+//const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+//
+//void setup() {
+//   Serial.begin(9600); // Starting Serial Terminal
+//}
+//
+//void loop() {
+//   long duration, inches, cm;
+//   pinMode(pingPin, OUTPUT);
+//   digitalWrite(pingPin, LOW);
+//   delayMicroseconds(2);
+//   digitalWrite(pingPin, HIGH);
+//   delayMicroseconds(10);
+//   digitalWrite(pingPin, LOW);
+//   pinMode(echoPin, INPUT);
+//   duration = pulseIn(echoPin, HIGH);
+//   inches = microsecondsToInches(duration);
+//   cm = microsecondsToCentimeters(duration);
+//   Serial.print(inches);
+//   Serial.print("in, ");
+//   Serial.print(cm);
+//   Serial.print("cm");
+//   Serial.println();
+//   delay(1000);
+//}
+//
+//long microsecondsToInches(long microseconds) {
+//   return microseconds / 74 / 2;
+//}
+//
+//long microsecondsToCentimeters(long microseconds) {
+//   return microseconds / 29 / 2;
+//}
