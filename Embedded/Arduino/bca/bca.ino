4bfd2e3de25420987af5e3df7b70a3f079b47f1e
JWooKim <lionmini@naver.com>
2020-11-04 23:02:52 +0900

    add bca in arduino

--- Embedded/Arduino/bca/bca.ino
+++ Embedded/Arduino/bca/bca.ino
@@ -15,7 +15,11 @@
 #define front_echo 33
 #define front_trig 37
 
-float reflect_duration, obstacle_distance, velocity, ttc;
+#define back_echo 41
+#define back_trig 45
+
+float reflect_duration, front_obstacle_distance, back_obstacle_distance;
+float velocity, ttc;
 float system_delay = 0.5;
 const float max_vel = 250;
 const float ppr = 1800;
@@ -25,7 +29,7 @@ volatile int rpmL, rpmR, rpm;
 void print_status()
 {
     Serial.print("distance : ");
-    Serial.println(obstacle_distance);
+    Serial.println(front_obstacle_distance);
 //    Serial.print("rpmL : ");
 //    Serial.println(rpmL);
 //    Serial.print("rpmR : ");
@@ -40,7 +44,7 @@ float calculate_ttc()
 {
   float _ttc;
   velocity = (rpm * 6.6 * 3.141592) / 60;
-  _ttc = (obstacle_distance / velocity) - 1;
+  _ttc = (back_obstacle_distance / velocity) - 1;
   if(_ttc < 0) _ttc = 0;
   return _ttc;
 }
@@ -51,35 +55,38 @@ void speedSetup(int left, int right)
     analogWrite(EB, left);
 }
 
-float detect_distance()
+void detect_distance()
 {
-    float _distance;
     digitalWrite(front_trig, LOW);
+    digitalWrite(back_trig, LOW);
     delayMicroseconds(2);
     digitalWrite(front_trig, HIGH);
+    digitalWrite(back_trig, HIGH);
     delayMicroseconds(10);
     digitalWrite(front_trig, LOW);
+    digitalWrite(back_trig, LOW);
     reflect_duration = pulseIn(front_echo, HIGH);
-    _distance = ((float)(340 * reflect_duration) / 10000) / 2;
-    return _distance;
+    front_obstacle_distance = ((float)(340 * reflect_duration) / 10000) / 2;
+    reflect_duration = pulseIn(back_echo, HIGH);
+    back_obstacle_distance = ((float)(340 * reflect_duration) / 10000) / 2;
 }
 
 void aeb_handler()
 {
-    if(obstacle_distance <= 30)
+    if(back_obstacle_distance <= 30)
     {
       ttc = calculate_ttc();
       if(ttc <= (1.5 + system_delay))
       {
-          speedSetup(0, 0);
-          while(1)
+        speedSetup(0, 0);
+        while(1)
+        {
+          detect_distance();
+          if(back_obstacle_distance > 40) 
           {
-            obstacle_distance = detect_distance();
-            if(obstacle_distance > 40) 
-            {
-              break;
-            }
+            break;
           }
+        }
       }
     }
 }
@@ -111,6 +118,8 @@ void setup()
 
     pinMode(front_trig, OUTPUT);
     pinMode(front_echo, INPUT);
+    pinMode(back_trig, OUTPUT);
+    pinMode(back_echo, INPUT);
     
     attachInterrupt(digitalPinToInterrupt(encoderL), pulseCounterL, RISING);
     attachInterrupt(digitalPinToInterrupt(encoderR), pulseCounterR, RISING);
@@ -118,26 +127,26 @@ void setup()
     MsTimer2::set(500, speedCalibration);
     MsTimer2::start();
 
-    digitalWrite(A2, HIGH);
-    digitalWrite(A1, LOW);
-    digitalWrite(B4, HIGH);
-    digitalWrite(B3, LOW);
+    digitalWrite(A2, LOW);
+    digitalWrite(A1, HIGH);
+    digitalWrite(B4, LOW);
+    digitalWrite(B3, HIGH);
 
     Serial.begin(57600);
 }
 
 void loop()
 {
-  obstacle_distance = detect_distance();
-  if(obstacle_distance > 80)
+  detect_distance();
+  if(back_obstacle_distance >= 80)
   {
     speedSetup(max_vel, max_vel);
   }
-  else if(obstacle_distance < 80 and obstacle_distance >= 60)
+  else if(back_obstacle_distance < 80 and back_obstacle_distance >= 60)
   {
     speedSetup((max_vel / 25) * 17, (max_vel / 25) * 17);
   }
-  else if(obstacle_distance < 60 and obstacle_distance >= 40)
+  else if(back_obstacle_distance < 60 and back_obstacle_distance >= 40)
   {
     speedSetup((max_vel / 25) * 13, (max_vel / 25) * 13);
   }
@@ -146,6 +155,5 @@ void loop()
     speedSetup((max_vel / 25) * 9, (max_vel / 25) * 9);
     aeb_handler();
   }
-  
   print_status();
 }
